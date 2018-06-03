35bae058bce7bb6e0d1f67cbc107d5819915f33e
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-06-03 11:11:09 +0100

    Switch to handle and select

--- src/Control/Selective.hs
+++ src/Control/Selective.hs
@@ -2,22 +2,29 @@ module Control.Selective where
 
 -- Selective applicative functor.
 --
--- Laws: 1) f <?*> fmap Left  x == f <*> x
---       2) f <?*> fmap Right x == x
+-- Laws: 1) handle f (fmap Left  x) == f <*> x  (free teorem)
+--       2) handle f (fmap Right x) == x
+--
+--       3) select f g (fmap Left  x) == f <*> x
+--       4) select f g (fmap Right x) == g <*> x
 --
 class Applicative f => Selective f where
-    (<?*>) :: f (a -> b) -> f (Either a b) -> f b
+    handle :: f (a -> b) -> f (Either a b) -> f b
+    handle f = select f (pure id)
+
+    select :: f (a -> c) -> f (b -> c) -> f (Either a b) -> f c
+    select l r x = handle r (handle (fmap (fmap Right) l) (fmap (fmap Left) x))
 
 -- This could be moved to the type class
-(<*?>) :: Selective f => f (Either a b) -> f (b -> a) -> f a
-x <*?> f = f <?*> fmap mirror x
+handleRight :: Selective f => f (b -> a) -> f (Either a b) -> f a
+handleRight f = handle f . fmap mirror
   where
     mirror (Left  x) = Right x
     mirror (Right x) = Left  x
 
 -- We can recover <*> from <?*>
 apS :: Selective f => f (a -> b) -> f a -> f b
-apS f x = f <?*> fmap Left x
+apS f = handle f . fmap Left
 
 -- This is the only possible implementation of <?*> using Applicative
 -- It always performs f's effects
@@ -25,26 +32,30 @@ selectA :: Applicative f => f (a -> b) -> f (Either a b) -> f b
 selectA f x = either <$> f <*> pure id <*> x
 
 -- Any Monad is Selective
-selectM :: Monad f => f (a -> b) -> f (Either a b) -> f b
-selectM mf mx = do
+handleM :: Monad f => f (a -> b) -> f (Either a b) -> f b
+handleM mf mx = do
     x <- mx
     case x of
         Left  a -> fmap ($a) mf
         Right b -> return b
 
+selectM :: Monad f => f (a -> c) -> f (b -> c) -> f (Either a b) -> f c
+selectM mf mg mx = do
+    x <- mx
+    case x of
+        Left  a -> fmap ($a) mf
+        Right b -> fmap ($b) mg
+
 -- Many useful Monad combinators that can be implemented with Selective
 
 whenS :: Selective f => f Bool -> f () -> f ()
-whenS x act = act *> pure id <?*> fmap (\b -> if b then Left () else Right ()) x
+whenS x act = handle (act *> pure id) (fmap (\b -> if b then Left () else Right ()) x)
 
 fromMaybeS :: Selective f => f a -> f (Maybe a) -> f a
-fromMaybeS x m = fmap const x <?*> fmap (maybe (Left ()) Right) m
-
-eitherS :: Selective f => f (a -> c) -> f (b -> c) -> f (Either a b) -> f c
-eitherS l r x = r <?*> (fmap (fmap Right) l <?*> fmap (fmap Left) x)
+fromMaybeS x = handle (fmap const x) . fmap (maybe (Left ()) Right)
 
 ifS :: Selective f => f Bool -> f a -> f a -> f a
-ifS i t e = eitherS (fmap const t) (fmap const e) $
+ifS i t e = select (fmap const t) (fmap const e) $
     fmap (\b -> if b then Left () else Right ()) i
 
 whileS :: Selective f => f Bool -> f ()
@@ -60,4 +71,5 @@ whileS act = whenS act (whileS act)
 
 -- Try: ite (pure True) (print 1) (print 2)
 instance Selective IO where
-    (<?*>) = selectM
+    handle = handleM
+    select = selectM
