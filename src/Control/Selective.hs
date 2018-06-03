62ec60f13bae9549b40188ed8fdb7311283dc778
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-06-03 02:23:47 +0100

    Add more functions

--- src/Control/Selective.hs
+++ src/Control/Selective.hs
@@ -16,13 +16,23 @@ x <*?> f = f <?*> fmap mirror x
     mirror (Right x) = Left  x
 
 -- We can recover <*> from <?*>
-apply :: Selective f => f (a -> b) -> f a -> f b
-apply f x = f <?*> fmap Left x
+apS :: Selective f => f (a -> b) -> f a -> f b
+apS f x = f <?*> fmap Left x
 
 -- This is the only possible implementation of <?*> using Applicative
 -- It always performs f's effects
-select :: Applicative f => f (a -> b) -> f (Either a b) -> f b
-select f x = either <$> f <*> pure id <*> x
+selectA :: Applicative f => f (a -> b) -> f (Either a b) -> f b
+selectA f x = either <$> f <*> pure id <*> x
+
+-- Any Monad is Selective
+selectM :: Monad f => f (a -> b) -> f (Either a b) -> f b
+selectM mf mx = do
+    x <- mx
+    case x of
+        Left  a -> fmap ($a) mf
+        Right b -> return b
+
+-- Many useful Monad combinators that can be implemented with Selective
 
 whenS :: Selective f => f Bool -> f () -> f ()
 whenS x act = act *> pure id <?*> fmap (\b -> if b then Left () else Right ()) x
@@ -33,16 +43,21 @@ fromMaybeS x m = fmap const x <?*> fmap (maybe (Left ()) Right) m
 eitherS :: Selective f => f (a -> c) -> f (b -> c) -> f (Either a b) -> f c
 eitherS l r x = r <?*> (fmap (fmap Right) l <?*> fmap (fmap Left) x)
 
-ite :: Selective f => f Bool -> f a -> f a -> f a
-ite i t e = eitherS (fmap const t) (fmap const e) $
+ifS :: Selective f => f Bool -> f a -> f a -> f a
+ifS i t e = eitherS (fmap const t) (fmap const e) $
     fmap (\b -> if b then Left () else Right ()) i
+
+whileS :: Selective f => f Bool -> f ()
+whileS act = whenS act (whileS act)
+
+(||^) :: Selective f => f Bool -> f Bool -> f Bool
+(||^) a b = ifS a (pure True) b
+
+(&&^) :: Selective f => f Bool -> f Bool -> f Bool
+(&&^) a b = ifS a b (pure False)
 
 -- Instances
 
 -- Try: ite (pure True) (print 1) (print 2)
 instance Selective IO where
-    mf <?*> mx = do
-        x <- mx
-        case x of
-            Left  a -> fmap ($a) mf
-            Right b -> return b
+    (<?*>) = selectM
