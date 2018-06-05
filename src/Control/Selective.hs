92d3148a19a2b377d011baf427371cac157a5312
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-06-06 00:47:18 +0100

    Tweak comments

--- src/Control/Selective.hs
+++ src/Control/Selective.hs
@@ -27,16 +27,19 @@ import qualified Data.Set as Set
 -- @b@ from @Right b@. Intuitively, 'handle' allows you to efficiently handle an
 -- error, which we often represent by @Left a@ in Haskell.
 --
+-- Note: the laws are still in flux. They still look unsatisfactory, so any ideas
+-- on how to improve them yet keep Const and Validation instances are welcome!
+--
 -- Law: If fmap Left x /= fmap Right x then
 --      * handle f (fmap Left  x) == f <*> x
 --      * handle f (fmap Right x) == x
 --
 -- For example, when f = Maybe we have:
---    * handle f (Just (Left  a)) == f <*> x
---    * handle f (Just (Right b)) == x
---    * handle f Nothing is not constrained, allowing the implementation to
---      select between the two above behaviours. The default implementation
---      provided for a Monad f skips the effect, i.e. handle f Nothing = Nothing.
+--      * handle f (Just (Left  a)) == f <*> x
+--      * handle f (Just (Right b)) == x
+--      * handle f Nothing is not constrained, allowing the implementation to
+--        select between the two above behaviours. The default implementation
+--        provided for a Monad f skips the effect: handle f Nothing == Nothing.
 class Applicative f => Selective f where
     handle :: f (a -> b) -> f (Either a b) -> f b
     default handle :: Monad f => f (a -> b) -> f (Either a b) -> f b
