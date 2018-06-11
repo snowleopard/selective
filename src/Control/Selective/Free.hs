8f7335ba349920d995ba43ea800974598ee6cef8
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-06-11 16:07:29 +0100

    Fix order of arguments in Free

--- src/Control/Selective/Free.hs
+++ src/Control/Selective/Free.hs
@@ -5,29 +5,31 @@ module Control.Selective.Free (
     ) where
 
 import Data.Bifunctor
+import Control.Selective
 
 -- Inspired by free applicative functors by Capriotti and Kaposi
 -- Beware of bugs in this code; I have only compiled it, not proved it correct.
 data Select f a where
     Pure   :: a -> Select f a
-    Handle :: Select f (a -> b) -> f (Either a b) -> Select f b
+    Handle :: f (Either a b) -> Select f (a -> b) -> Select f b
 
 instance Functor f => Functor (Select f) where
     fmap f (Pure x)     = Pure (f x)
-    fmap f (Handle g x) = Handle (fmap (fmap f) g) (fmap (fmap f) x)
+    fmap f (Handle x g) = Handle (fmap (fmap f) x) (fmap (fmap f) g)
 
 instance Functor f => Applicative (Select f) where
     pure  = Pure
     (<*>) = apS
 
 liftSelect :: Functor f => f a -> Select f a
-liftSelect fa = Handle (Pure id) (fmap Left fa)
+liftSelect fa = Handle (fmap Left fa) (Pure id)
 
 instance Functor f => Selective (Select f) where
-    handle (Pure f) x     = fmap (either f id) x
-    handle (Handle f x) y = handle (fmap uncurry f) $ handle
-        (liftSelect $ fmap (\x a -> bimap (,a) ($a) x) x) (fmap (fmap Right) y)
+    handle x (Pure f)     = fmap (either f id) x
+    handle x (Handle y f) = handle
+        (handle (fmap (fmap Right) x) (liftSelect $ fmap (\x a -> bimap (,a) ($a) x) y))
+        (fmap uncurry f)
 
 runSelect :: Selective g => (forall a. f a -> g a) -> Select f a -> g a
 runSelect _ (Pure a)     = pure a
-runSelect t (Handle f x) = handle (runSelect t f) (t x)
+runSelect t (Handle x f) = handle (t x) (runSelect t f)
