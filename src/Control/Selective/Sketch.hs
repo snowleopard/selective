e69a6243e7dd26e3b783e6d701e6eff2d684e752
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-06-25 03:22:42 +0100

    Simplify proof notation

--- src/Control/Selective/Sketch.hs
+++ src/Control/Selective/Sketch.hs
@@ -18,48 +18,31 @@ infixl 0 ===
 
 -- F1: f <$> handle x y = handle (second f <$> x) ((f .) <$> y)
 f1 :: Selective f => (b -> c) -> f (Either a b) -> f (a -> b) -> f c
-f1 f x y = lhs === rhs
-  where
-    lhs = f <$> handle x y
-    rhs = handle (second f <$> x) ((f .) <$> y)
+f1 f x y = f <$> handle x y === handle (second f <$> x) ((f .) <$> y)
 
 -- F2: handle (first f <$> x) y = handle x ((. f) <$> y)
 f2 :: Selective f => (a -> c) -> f (Either a b) -> f (c -> b) -> f b
-f2 f x y = lhs === rhs
-  where
-    lhs = handle (first f <$> x) y
-    rhs = handle x ((. f) <$> y)
+f2 f x y = handle (first f <$> x) y === handle x ((. f) <$> y)
 
 -- F3: handle x (f <$> y) = handle (first (flip f) <$> x) (flip ($) <$> y)
 f3 :: Selective f => (c -> a -> b) -> f (Either a b) -> f c -> f b
-f3 f x y = lhs === rhs
-  where
-    lhs = handle x (f <$> y)
-    rhs = handle (first (flip f) <$> x) (flip ($) <$> y)
+f3 f x y = handle x (f <$> y) === handle (first (flip f) <$> x) (flip ($) <$> y)
 
 -- P1: handle x (pure y) == either y id <$> x
 p1 :: Selective f => f (Either a b) -> (a -> b) -> f b
-p1 x y = lhs === rhs
-  where
-    lhs = handle x (pure y)
-    rhs = either y id <$> x
+p1 x y = handle x (pure y) === either y id <$> x
 
 -- P2: handle (pure (Left x)) y = ($x) <$> y
 p2 :: Selective f => a -> f (a -> b) -> f b
-p2 x y = lhs === rhs
-  where
-    lhs = handle (pure (Left x)) y
-    rhs = ($x) <$> y
+p2 x y = handle (pure (Left x)) y === ($x) <$> y
 
 -- A1: handle x (handle y z) = handle (handle (f <$> x) (g <$> y)) (h <$> z)
 --       where f x = Right <$> x
 --             g y = \a -> bimap (,a) ($a) y
 --             h z = uncurry z
 a1 :: Selective f => f (Either a b) -> f (Either c (a -> b)) -> f (c -> a -> b) -> f b
-a1 x y z = lhs === rhs
+a1 x y z = handle x (handle y z) === handle (handle (f <$> x) (g <$> y)) (h <$> z)
   where
-    lhs = handle x (handle y z)
-    rhs = handle (handle (f <$> x) (g <$> y)) (h <$> z)
     f x = Right <$> x
     g y = \a -> bimap (,a) ($a) y
     h z = uncurry z
@@ -68,105 +51,97 @@ a1 x y z = lhs === rhs
 
 -- Identity: pure id <*> v = v
 t1 :: Selective f => f a -> f a
-t1 v = s1 === s2 === s3 === s4 === s5 === s6
-  where
-    -- Step 1: Express the lefthand side using 'apS'
-    s1 = apS (pure id) v
-    -- Step 2: Definition of 'apS'
-    s2 = handle (Left <$> pure id) (flip ($) <$> v)
-    -- Step 3: Push 'Left' inside 'pure'
-    s3 = handle (pure (Left id)) (flip ($) <$> v)
-    -- Step 4: Apply P2
-    s4 = ($id) <$> (flip ($) <$> v)
-    -- Step 5: Simplify
-    s5 = id <$> v
-    -- Step 6: Functor identity: fmap id = id
-    s6 = v
+t1 v =
+    -- Express the lefthand side using 'apS'
+    apS (pure id) v
+    === -- Definition of 'apS'
+    handle (Left <$> pure id) (flip ($) <$> v)
+    === -- Push 'Left' inside 'pure'
+    handle (pure (Left id)) (flip ($) <$> v)
+    === -- Apply P2
+    ($id) <$> (flip ($) <$> v)
+    === -- Simplify
+    id <$> v
+    === -- Functor identity: fmap id = id
+    v
 
 -- Homomorphism: pure f <*> pure x = pure (f x)
 t2 :: Selective f => (a -> b) -> a -> f b
-t2 f x = s1 === s2 === s3 === s4 === s5 === s6
-  where
-    -- Step 1: Express the lefthand side using 'apS'
-    s1 = apS (pure f) (pure x)
-    -- Step 2: Definition of 'apS'
-    s2 = handle (Left <$> pure f) (flip ($) <$> pure x)
-    -- Step 3: Push 'Left' inside 'pure'
-    s3 = handle (pure (Left f)) (flip ($) <$> pure x)
-    -- Step 4: Apply P2
-    s4 = ($f) <$> (flip ($) <$> pure x)
-    -- Step 5: Simplify
-    s5 = f <$> pure x
-    -- Step 6: Apply a pure function to a pure value (pure f <*> pure x)
-    s6 = pure (f x)
+t2 f x =
+    -- Express the lefthand side using 'apS'
+    apS (pure f) (pure x)
+    === -- Definition of 'apS'
+    handle (Left <$> pure f) (flip ($) <$> pure x)
+    === -- Push 'Left' inside 'pure'
+    handle (pure (Left f)) (flip ($) <$> pure x)
+    === -- Apply P2
+    ($f) <$> (flip ($) <$> pure x)
+    === -- Simplify
+    f <$> pure x
+    === -- Apply a pure function to a pure value (pure f <*> pure x)
+    pure (f x)
 
 -- Interchange: u <*> pure y = pure ($y) <*> u
 t3 :: Selective f => f (a -> b) -> a -> f b
-t3 u y = s1 === s2 === s3 === s4 === s5 === s6 === s7 === s8 === s9 === s10
-  where
-    -- Step 1: Express the lefthand side using 'apS'
-    s1 = apS u (pure y)
-    -- Step 2: Definition of 'apS'
-    s2 = handle (Left <$> u) (flip ($) <$> pure y)
-    -- Step 3: Rewrite to have a pure handler
-    s3 = handle (Left <$> u) (pure ($y))
-    -- Step 4: Apply P1
-    s4 = either ($y) id <$> (Left <$> u)
-    -- Step 5: Simplify
-    s5 = ($y) <$> u
+t3 u y =
+    -- Express the lefthand side using 'apS'
+    apS u (pure y)
+    === -- Definition of 'apS'
+    handle (Left <$> u) (flip ($) <$> pure y)
+    === -- Rewrite to have a pure handler
+    handle (Left <$> u) (pure ($y))
+    === -- Apply P1
+    either ($y) id <$> (Left <$> u)
+    === -- Simplify, obtaining (#)
+    ($y) <$> u
 
-    -- Step 6: Express right-hand side of the theorem using 'apS'
-    s6 = apS (pure ($y)) u
-    -- Step 7: Definition of 'apS'
-    s7 = handle (Left <$> pure ($y)) (flip ($) <$> u)
-    -- Step 8: Push 'Left' inside 'pure'
-    s8 = handle (pure (Left ($y))) (flip ($) <$> u)
-    -- Step 9: Apply P2
-    s9 = ($($y)) <$> (flip ($) <$> u)
-    -- Step 10: Simplify, obtaining the 's10' = 's5'.
-    s10 = ($y) <$> u
+    === -- Express right-hand side of the theorem using 'apS'
+    apS (pure ($y)) u
+    === -- Definition of 'apS'
+    handle (Left <$> pure ($y)) (flip ($) <$> u)
+    === -- Push 'Left' inside 'pure'
+    handle (pure (Left ($y))) (flip ($) <$> u)
+    === -- Apply P2
+    ($($y)) <$> (flip ($) <$> u)
+    === -- Simplify, obtaining (#)
+    ($y) <$> u
 
 -- Composition: (.) <$> u <*> v <*> w = u <*> (v <*> w)
 t4 :: Selective f => f (b -> c) -> f (a -> b) -> f a -> f c
-t4 u v w = lhs === rhs
-  where
-    -- Rewrite the lefthand side:
-    lhs = s1 === s2 === s3 === s4 === s5 === s6 === s7
-    -- Step 1: Express the lefthand side using 'apS'
-    s1 = apS (apS ((.) <$> u) v) w
-    -- Step 2: Definition of 'apS'
-    s2 = handle (Left <$> handle (Left <$> (.) <$> u) (flip ($) <$> v)) (flip ($) <$> w)
-    -- Step 3: Apply F1 to push the leftmost 'Left' inside 'handle'
-    s3 = handle (handle (second Left <$> Left <$> (.) <$> u) ((Left .) <$> flip ($) <$> v)) (flip ($) <$> w)
-    -- Step 4: Simplify
-    s4 = handle (handle (Left <$> (.) <$> u) ((Left .) <$> flip ($) <$> v)) (flip ($) <$> w)
-    -- Step 5: Pull (.) outside 'Left'
-    s5 = handle (handle (first (.) <$> Left <$> u) ((Left .) <$> flip ($) <$> v)) (flip ($) <$> w)
-    -- Step 6: Apply F2 to push @(.)@ to the handler
-    s6 = handle (handle (Left <$> u) ((. (.)) <$> (Left .) <$> flip ($) <$> v)) (flip ($) <$> w)
-    -- Step 7: Simplify
-    s7 = handle (handle (Left <$> u) ((Left .) <$> flip (.) <$> v)) (flip ($) <$> w)
+t4 u v w =
+    -- Express the lefthand side using 'apS'
+    apS (apS ((.) <$> u) v) w
+    === -- Definition of 'apS'
+    handle (Left <$> handle (Left <$> (.) <$> u) (flip ($) <$> v)) (flip ($) <$> w)
+    === -- Apply F1 to push the leftmost 'Left' inside 'handle'
+    handle (handle (second Left <$> Left <$> (.) <$> u) ((Left .) <$> flip ($) <$> v)) (flip ($) <$> w)
+    === -- Simplify
+    handle (handle (Left <$> (.) <$> u) ((Left .) <$> flip ($) <$> v)) (flip ($) <$> w)
+    === -- Pull (.) outside 'Left'
+    handle (handle (first (.) <$> Left <$> u) ((Left .) <$> flip ($) <$> v)) (flip ($) <$> w)
+    === -- Apply F2 to push @(.)@ to the handler
+    handle (handle (Left <$> u) ((. (.)) <$> (Left .) <$> flip ($) <$> v)) (flip ($) <$> w)
+    === -- Simplify, obtaining (#)
+    handle (handle (Left <$> u) ((Left .) <$> flip (.) <$> v)) (flip ($) <$> w)
 
-    -- Rewrite the righthand side:
-    rhs = s8 === s9 === s10 === s11 === s12 === s13 === s14 === s15 === s16
-    -- Step 8: Express the righthand side using 'apS'
-    s8 = apS u (apS v w)
-    -- Step 9: Definition of 'apS'
-    s9 = handle (Left <$> u) (flip ($) <$> handle (Left <$> v) (flip ($) <$> w))
-    -- Step 10: Apply F1 to push @flip ($)@ inside 'handle'
-    s10 = handle (Left <$> u) (handle (Left <$> v) ((flip ($) .) <$> flip ($) <$> w))
-    -- Step 11: Apply A1 to reassociate to the left
-    s11 = handle (handle (Left <$> u) ((\y a -> bimap (,a) ($a) y) <$> Left <$> v)) (uncurry . (flip ($) .) <$> flip ($) <$> w)
-    -- Step 12: Simplify
-    s12 = handle (handle (Left <$> u) ((\y a -> Left (y, a)) <$> v)) ((\x (f, g) -> g (f x)) <$> w)
-    -- Step 13: Apply F3 to pull the rightmost pure function inside 'handle'
-    s13 = handle (first (flip ((\x (f, g) -> g (f x)))) <$> handle (Left <$> u) ((\y a -> Left (y, a)) <$> v)) (flip ($) <$> w)
-    -- Step 14: Simplify
-    s14 = handle (first (\(f, g) -> g . f) <$> handle (Left <$> u) ((\y a -> Left (y, a)) <$> v)) (flip ($) <$> w)
-    -- Step 15: Apply F1 to push the leftmost pure function inside 'handle'
-    s15 = handle (handle (Left <$> u) (((first (\(f, g) -> g . f)).) <$> (\y a -> Left (y, a)) <$> v)) (flip ($) <$> w)
-    -- Step 16: Simplify, obtaining the 's16' = 's7'.
-    s16 = handle (handle (Left <$> u) ((Left .) <$> flip (.) <$> v)) (flip ($) <$> w)
+    === -- Express the righthand side using 'apS'
+    apS u (apS v w)
+    === -- Definition of 'apS'
+    handle (Left <$> u) (flip ($) <$> handle (Left <$> v) (flip ($) <$> w))
+    === -- Apply F1 to push @flip ($)@ inside 'handle'
+    handle (Left <$> u) (handle (Left <$> v) ((flip ($) .) <$> flip ($) <$> w))
+    === -- Apply A1 to reassociate to the left
+    handle (handle (Left <$> u) ((\y a -> bimap (,a) ($a) y) <$> Left <$> v)) (uncurry . (flip ($) .) <$> flip ($) <$> w)
+    === -- Simplify
+    handle (handle (Left <$> u) ((\y a -> Left (y, a)) <$> v)) ((\x (f, g) -> g (f x)) <$> w)
+    === -- Apply F3 to pull the rightmost pure function inside 'handle'
+    handle (first (flip ((\x (f, g) -> g (f x)))) <$> handle (Left <$> u) ((\y a -> Left (y, a)) <$> v)) (flip ($) <$> w)
+    === -- Simplify
+    handle (first (\(f, g) -> g . f) <$> handle (Left <$> u) ((\y a -> Left (y, a)) <$> v)) (flip ($) <$> w)
+    === -- Apply F1 to push the leftmost pure function inside 'handle'
+    handle (handle (Left <$> u) (((first (\(f, g) -> g . f)).) <$> (\y a -> Left (y, a)) <$> v)) (flip ($) <$> w)
+    === -- Simplify, obtaining (#)
+    handle (handle (Left <$> u) ((Left .) <$> flip (.) <$> v)) (flip ($) <$> w)
 
 --------------------------------- End of proofs --------------------------------
 
