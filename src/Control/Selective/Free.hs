2f048f72a7529fe5432c156ddc5866bb3eee2ad4
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-06-27 01:24:41 +0100

    Minor revision

--- src/Control/Selective/Free.hs
+++ src/Control/Selective/Free.hs
@@ -7,7 +7,7 @@ module Control.Selective.Free (
 import Data.Bifunctor
 import Control.Selective
 
--- Inspired by free applicative functors by Capriotti and Kaposi
+-- Inspired by free applicative functors by Capriotti and Kaposi.
 -- Beware of bugs in this code; I have only compiled it, not proved it correct.
 data Select f a where
     Pure   :: a -> Select f a
@@ -15,20 +15,20 @@ data Select f a where
 
 instance Functor f => Functor (Select f) where
     fmap f (Pure x)     = Pure (f x)
-    fmap f (Handle x g) = Handle (fmap (fmap f) x) (fmap (fmap f) g)
+    fmap f (Handle x y) = Handle (second f <$> x) ((f .) <$> y) -- Law F1
 
 instance Functor f => Applicative (Select f) where
     pure  = Pure
     (<*>) = apS
 
 liftSelect :: Functor f => f a -> Select f a
-liftSelect fa = Handle (fmap Left fa) (Pure id)
+liftSelect fa = Handle (Left <$> fa) (Pure id) -- Law P1
 
 instance Functor f => Selective (Select f) where
-    handle x (Pure f)     = fmap (either f id) x
-    handle x (Handle y f) = handle
-        (handle (fmap (fmap Right) x) (liftSelect $ fmap (\x a -> bimap (,a) ($a) x) y))
-        (fmap uncurry f)
+    handle x (Pure y)     = either y id <$> x -- Law P1
+    handle x (Handle y z) = handle -- Law A1
+        (handle (fmap Right <$> x) (liftSelect $ (\k a -> bimap (,a) ($a) k) <$> y))
+        (uncurry <$> z)
 
 runSelect :: Selective g => (forall a. f a -> g a) -> Select f a -> g a
 runSelect _ (Pure a)     = pure a
