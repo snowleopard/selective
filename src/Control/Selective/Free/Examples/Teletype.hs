61ed329c3072e3ccb10c89ef6d75e31bea1d2d43
Georgy Lukyanov <georgiylukjanov@gmail.com>
2019-02-25 14:37:48 +0000

    Simplify the PingPong example and remname it to Teletype.

--- src/Control/Selective/Free/Examples/Teletype.hs
+++ src/Control/Selective/Free/Examples/Teletype.hs
@@ -8,45 +8,34 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DerivingVia #-}
 
-module Control.Selective.Free.Examples.PingPong where
+module Control.Selective.Free.Examples.Teletype where
 
 import Prelude hiding (getLine, putStrLn)
 import qualified Prelude (getLine, putStrLn)
 import Control.Selective
 import Control.Selective.Free
 
--- | The 'RW' (pronounced "Read-Write") functor is abstracts a key-value store.
-data RW k v a = Read  k   (v -> a)
-              -- ^ read a value 'v' associated to to the key 'k' and transform it to an 'a'.
-              | Write k v (v -> a)
-              -- ^ associate the value 'v' with the key 'k',
-              --   then transform the 'v' to an 'a' and return it as a result
+data TeletypeF a = GetLine (String -> a)
+                 | PutLine String a
     deriving Functor
 
-instance (Show k, Show v, Show a) => Show (RW k v a) where
-    show (Read k _)    = "Read "  ++ show k
-    show (Write k v _) = "Write " ++ show k ++ " " ++ show v
-
--- | A TeletypeF is an specialisation of the 'RW' with a singleton key datatype
---   and character strings as values
-newtype TeletypeF a = TeletypeF { runTeletypeF :: RW () String a }
-    deriving newtype Functor
-    deriving Show via (RW () String a)
-
+instance Show (TeletypeF a) where
+    show (GetLine _)   = "GetLine"
+    show (PutLine s _) = "PutLine " ++ s
 
 -- | Interpret the 'TeletypeF' commands as IO actions
 inIO :: TeletypeF a -> IO a
-inIO (TeletypeF (Read _ t))    = t <$> Prelude.getLine
-inIO (TeletypeF (Write _ v t)) = t <$> (Prelude.putStrLn v *> pure v)
+inIO (GetLine t)   = t <$> Prelude.getLine
+inIO (PutLine v x) = Prelude.putStrLn v *> pure x
 
 -- | A Teletype program is a free Selective over the 'TeletypeF' functor
 type Teletype a = Select TeletypeF a
 
 getLine :: Teletype String
-getLine = liftSelect (TeletypeF $ Read () id)
+getLine = liftSelect (GetLine id)
 
 putStrLn :: String -> Teletype ()
-putStrLn v = liftSelect (TeletypeF $ Write () v (const ()))
+putStrLn v = liftSelect (PutLine v ())
 
 -- | The example from the paper's intro. Implemented in terms of the free
 --   selective, can now be statically analysed for necessary effects:
@@ -64,4 +53,3 @@ putStrLn v = liftSelect (TeletypeF $ Write () v (const ()))
 --   <no responce>
 pingPong :: Teletype ()
 pingPong = whenS (fmap (=="ping") getLine) (putStrLn "pong")
-
